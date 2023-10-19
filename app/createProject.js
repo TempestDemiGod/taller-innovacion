8058a1f1e66807003e3c942a195ab5376806ce5d
DemiGod <119078964+TempestDemiGod@users.noreply.github.com>
2023-10-19 17:33:37 -0500

    genesis de tempest

---
+++ app/createProject.js
@@ -0,0 +1,115 @@
+// const acordeon = document.querySelector('#accordionFlushExample')
+
+// const btnAdd = document.querySelector('#addProject')
+
+// btnAdd.addEventListener('click', ()=>{
+//     const projectnew = document.createElement('div')
+//     projectnew.classList.add('accordion-item')
+//     projectnew.innerHTML = `
+//     <h2 class="accordion-header" id="flush-headingOne">
+//               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+//                 data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+//                 Proyecto taller
+//               </button>
+//             </h2>
+//             <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+//               data-bs-parent="#accordionFlushExample">
+//               <button class="form-control">proyecto</button>
+//               <button class="form-control" disabled>mapa de afinidad</button>
+//               <button class="form-control" disabled>brainstorming</button>
+//               <button class="form-control" disabled>mapa de inpacto</button>
+//             </div>
+//     `
+//     acordeon.appendChild(projectnew)
+// })
+import { collection, addDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"; 
+import { getDocs, doc} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
+// import {  } from '../js/generales/inicio.js'
+const btncreate = document.querySelector('#btncreate')
+const modalactiv = document.getElementById('modal')
+const btnclose = document.getElementById('close')
+const registerProject = document.getElementById('register-form-project');
+// window.addEventListener('DOMContentLoaded', async()=>{
+//   const querySnapshot = await obtProjects()
+//   console.log(querySnapshot)
+// })
+
+const showRegisterModal = ()=>{
+  modalactiv.classList.toggle('is-active')
+}
+try{
+  btnclose.addEventListener('click',showRegisterModal);
+btncreate.addEventListener('click',showRegisterModal);
+
+}catch{
+
+}
+function guardar() {
+  const nameProject = registerProject['name-project'].value;
+  const apiKeyProject = registerProject['apikey-project'].value;
+  const descProject = registerProject['description-project'].value;
+}
+
+
+try{
+  registerProject.addEventListener('submit',(e)=>{
+    e.preventDefault()
+  
+    const nameProject = registerProject['name-project'].value;
+    const apiKeyProject = registerProject['apikey-project'].value;
+    const temaProject = registerProject['tema-project'].value;
+    const descProject = registerProject['description-project'].value;
+  
+  
+    const guardar = async()=>{
+      const docRef = await addDoc(collection(db, "tallerInnovacion"), {
+        nameProject: nameProject,
+        apiKey: apiKeyProject,
+        temaProject: temaProject,
+        descProject: descProject,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      window.location.reload()
+    }
+    
+    guardar()
+    showRegisterModal()
+    // const registerProjects = projectRef.push()
+    // console.log(registerProjects)
+    // console.log(registerProjects.path.pieces_[1])
+    // registerProjects.set({
+    //   Uid: registerProject.path.pieces_[1]
+    // })
+  })
+}catch{
+
+}
+// -----------------------------------------------
+// ---------------------------------------------
+// ----------------------------------------------
+function printProjects(){
+  // console.log(projects)
+  // for(let i = 0; i<10;i++){
+  //   console.log('project')
+  // }
+  // projects.forEach((elemento) => {
+  //   console.log(elemento);
+  //   // Aquí puedes realizar cualquier acción que desees con cada elemento
+  // });
+}
+let keysProjects = []
+export async function incrust(listado){
+  keysProjects.push(listado)
+  console.log(keysProjects)
+}
+const btnProjectOpen = document.querySelector('.btnOpen1')
+const contProjectOpen = document.querySelector('.btn-active')
+const openOptions=()=>{
+  contProjectOpen.classList.toggle('d-none')
+}
+
+try{
+  btnProjectOpen.addEventListener('click',openOptions)
+}catch{
+  
+}
---
+++ app/firebase.js
@@ -0,0 +1,85 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js"
+import {getFirestore, collection, query, getDocs } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
+import {incrust}from  "./createProject.js"
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlrFZ7E7hU-3DPd8ykdDsaCoVu4Q9Ga3Y",
+  authDomain: "taller-proyecto-6190e.firebaseapp.com",
+  projectId: "taller-proyecto-6190e",
+  storageBucket: "taller-proyecto-6190e.appspot.com",
+  messagingSenderId: "677848867911",
+  appId: "1:677848867911:web:e56ce0c6cc12fdaef9bd62"
+};
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+
+export const db = getFirestore();
+
+const colRef = collection(db, 'tallerInnovacion')
+export async function dataKey(key){
+  let arrayKey = []
+  arrayKey.push(key)
+  console.log(arrayKey)
+  
+  await incrust(arrayKey)
+}
+
+const userList = getDocs(colRef)
+  .then((snapshot)=>{
+    const projectsControl = document.querySelector('#projects-control')
+    let projects= []
+    snapshot.docs.forEach((doc)=>{
+      projects.push({ ...doc.data(), id: doc.id})
+      let temProject = { ...doc.data(), id: doc.id}
+      const projectnew = document.createElement('div')
+      projectnew.classList.add('form-control')
+      projectnew.innerHTML = `
+            <button class="btn btn-outline-danger btnOpen1 btn form-control">
+            ${temProject.nameProject}
+            </button>
+            <div class=" container btn-active">
+              <button class="btn btn-outline-secondary form-control mt-3">
+                proyecto
+              </button>
+              <a href="./mapa_afinidad.html" style="text-decoration: none;"><button class="btn btn-outline-secondary form-control mt-3" >
+                mapa de afinidad
+              </button></a>
+              <a href="./mapa_empatia.html" style="text-decoration: none;"><button class="btn btn-outline-secondary form-control mt-3" >
+                mapa de empatia
+              </button></a>
+              <a href="./brainstorming.html" style="text-decoration: none;"><button class="btn btn-outline-secondary form-control mt-3" >
+                brainstorming
+              </button></a>
+              <a href="./mapa_impacto.html" style="text-decoration: none;"><button class="btn btn-outline-secondary form-control mt-3">
+                mapa de impacto
+              </button></a>
+              <!-- ----------- -->
+            </div>
+      
+      `;
+      console.log(temProject)
+      dataKey(temProject.apiKey)
+      projectsControl.appendChild(projectnew);
+    })
+    console.log(projects)
+    console.log(projects.length)
+
+  })
+  .catch(err=>{
+    console.log(err.message)
+  })
+  // projects.forEach((doc)=>{
+  //   console.log(doc)
+  // })console.log(projects.length)
+  // export async function lol (){
+  //   console.log(projects.length)
+  //   console.log(projects[0])
+  // }
+
+
+  console.log('lafe muchachosss :',userList)
