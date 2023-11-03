ee819151c2be4c881c95ea0f5b5334e4f4ff6617
luisvr201 <119078851+luisvr201@users.noreply.github.com>
2023-11-03 11:49:18 -0500

    Update configuraciongpt.js

--- js/User_Reseach/configuraciongpt.js
+++ js/User_Reseach/configuraciongpt.js
@@ -5,6 +5,7 @@ let prompt = document.getElementById('prompt');
     
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
+    console.log('--------------------la fe----------------')
     const tema = document.getElementById("gpt-Tema").value;
     console.log('submite')
     prompt.innerHTML = `Estoy creando un User Reseach. segun el tema: ${tema}\n Siguiendo el fremawork Lean Startup para realizar su entregable User Reseach del tema ${tema}, el resultado en españo`
